45760fd225218ba065060fdebfe219bd145c3c36
VincentLepetit <vlepetit@gmail.com>
2021-08-27 16:40:21 +0200

    Update custom_BibTex.js

--- custom_BibTex.js
+++ custom_BibTex.js
@@ -2,6 +2,8 @@
 // <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 // <script src="BibTex.js"></script>
 
+image_root = "https://vincentlepetit.github.io/"
+
 var expanded_venues = [
     "3DV",   "International Conference on 3D Vision",
     "ACCV",  "Proceedings of the Asian Conference on Computer Vision (<b>ACCV</b>)",
@@ -182,22 +184,6 @@ function bibtex2html_BibTex(bibtex_entries)
     }
 
     var ret = "";
-
-    const uaDataIsMobile = window.navigator.userAgentData?.mobile;
-    if (uaDataIsMobile) {
-	    ret = "MOBILE";
-    } else {
-   	    ret = "NOT MOBILE";
-    }
-    ret += navigator.userAgent;
-	
-    let check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-    if (check) {
-	    ret += "MOBILE2";
-    } else {
-	    ret += "NOT MOBILE2";
-    }
 	
     for (var current_year = max_year; current_year >= min_year; current_year--) {
 	ret += "<h2>\n" + current_year.toString() + "</h2>\n";
@@ -223,7 +209,7 @@ function bibtex2html_BibTex(bibtex_entries)
 		    if (weblink != '') {
 			ret += "<a href = \"" + weblink + "\">";
 		    }
-		    ret += "<img alt = \"<missing>\" width = 300 src = \"https://vincentlepetit.github.io/" + img + "\" class = \"thumbnail\" ></img>";
+		    ret += "<img alt = \"<missing>\" width = 300 src = \"" + image_root + "/" + img + "\" class = \"thumbnail\" ></img>";
 		    if (weblink != '') {
 			ret += "</a>";
 		    }
@@ -244,12 +230,79 @@ function bibtex2html_BibTex(bibtex_entries)
     return ret;
 }
 
+function bibtex2html_BibTex_on_mobile(bibtex_entries)
+{
+    var year = Number(extract(bibtex_entries.data[0], 'year'))
+    var min_year = year;
+    var max_year = year;
+    for (var i=0; i<bibtex_entries.data.length; i++){
+	year = Number(extract(bibtex_entries.data[i], 'year'))
+	if (year > max_year) { max_year = year; }
+	if (year < min_year) { min_year = year; }
+    }
+
+    var ret = "";
+	
+    for (var current_year = max_year; current_year >= min_year; current_year--) {
+	ret += "<h2>\n" + current_year.toString() + "</h2>\n";
+
+	ret += "<table id=\"publis\">\n";
+
+	for (var i=0; i<bibtex_entries.data.length; i++){
+	    var entry = bibtex_entries.data[i];
+	    var year = Number(extract(entry, 'year'));
+	    if (year == current_year) {
+		var img = extract(entry, 'img');
+		var weblink = extract(entry, 'weblink');
+		if (weblink == "") {
+		  weblink = extract(entry, 'pdf');
+		}
+		entry_html = entry2html(entry);
+		var anchor_html = "<a id=\"" + entry['cite'] + "\"></a>";		
+		ret += anchor_html;
+
+		ret += "<tr>\n";
+		if (img != '') {
+		    ret += "<td>";
+		    if (weblink != '') {
+			ret += "<a href = \"" + weblink + "\">";
+		    }
+		    ret += "<img alt = \"<missing>\" width = 300 src = \"" + image_root + "/" + img + "\" class = \"thumbnail\" ></img>";
+		    if (weblink != '') {
+			ret += "</a>";
+		    }
+		    ret += "</td></tr><tr><td>";
+		    ret += entry_html;
+		    ret += "</td></tr>\n";
+		} else {
+		    ret += "<td>";
+		    ret += entry_html;
+		    ret += "</td>";
+		}
+		ret += "</tr>\n";
+	    }
+	}
+	ret += "</table>\n";
+    }
+
+    return ret;
+}
+
+
 function bibtex2html_bibfile(bibfile_name, list_of_publications_id)
 {
     jQuery.get(bibfile_name, function(textString) {
 	bibtex_entries = new BibTex();
 	bibtex_entries.content = textString;
 	bibtex_entries.parse();
-	document.getElementById(list_of_publications_id).innerHTML = bibtex2html_BibTex(bibtex_entries);
+
+	let we_are_on_mobile = false;
+        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) we_are_on_mobile = true;})(navigator.userAgent||navigator.vendor||window.opera);
+//         if (we_are_on_mobile) {
+            document.getElementById(list_of_publications_id).innerHTML = bibtex2html_BibTex_on_mobile(bibtex_entries);
+//         } else {
+//             document.getElementById(list_of_publications_id).innerHTML = bibtex2html_BibTex(bibtex_entries);
+//         }
+	
     });
 }
